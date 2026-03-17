5114981ea9119921c649eab7913bdebc58a680ff
Anna Araujo <anna62211976@alunos.sc.senac.br>
2026-03-16 21:09:07 -0300

    Adicionado exercicios de tupla

---
+++ exerciciosTuplas.py
@@ -0,0 +1,21 @@
+#Crie uma tupla com os dias da semana.
+
+dias= ("Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado", "Domingo")
+print("-----------------------------------------")
+print("Os dias da semana são:", dias)
+print("-----------------------------------------")
+
+#Exibir o primeiro dia da semana.
+
+print("O primeiro dia da semana é:", dias[0])
+print("-----------------------------------------")
+
+#Exibir o último dia da semana.
+
+print("O último dia da semana é:", dias[-1])
+print("-----------------------------------------")
+
+#Tamanho da tupla.
+
+print("O tamanho da tupla é:", len(dias))
+print("-----------------------------------------")
